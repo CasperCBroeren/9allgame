0c4dafdc50e0fd5f2f528e39db29b986d5c1d3b1
Casper Broeren <c.broeren@gmail.com>
2018-12-13 10:54:50 +0100

    Removed code from template Used javascript interop to do the timer

---
+++ wwwroot/clientSideFunctions.js
@@ -0,0 +1,15 @@
+﻿window.clientSideFunctions = {
+ 
+    startTimer: function () {
+        var self = this;
+        this.timerElement = document.getElementById("timerElement");
+        this.seconds = 0;
+        this.timerHandler = setInterval(function () {
+            self.seconds += 1;
+            self.timerElement.innerText = self.seconds + " sec";
+        }, 1000);
+    },
+    stopTimer: function () {
+        clearInterval(this.timerHandler);
+    }
+}
